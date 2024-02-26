b50ee4ae641070cef6639b3f5802dc8f5a536bbe
flores.313371 <flores.313371@LAB-A-PC38>
2024-02-26 12:23:33 +0800

    Add project files.

---
+++ DataTypesApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DataTypesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the pieces of apple: ");
+            int apples = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the total price of " + apples + "apple(s): ");
+            double totalPrice = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The total price of " + apples + "apple(s) " + totalPrice);
+            Console.WriteLine("The value of original price is " + totalPrice);
+            int totalPrice1 = (int)totalPrice;
+            Console.WriteLine("The value of converted price is " + totalPrice1);
+
+        }
+    }
+}
